6ae2bdea129932dd6b1d3f031b07ef2277055e82
Belal-hossainn <belalhossain.rn@gmail.com>
2022-01-31 16:19:57 +0600

    initial setup & header done

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <h1 className="cursor-pointer text-lg font-bold tracking-wide">
+            Globe Notes
+          </h1>
+        </Link>
+        <div className="hidden cursor-pointer items-center space-x-4 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full border border-gray-400 px-4 py-1">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div>
+        <h3 className="cursor-pointer rounded-full border border-gray-400 px-3 py-1 hover:text-green-500">
+          Sign In
+        </h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
